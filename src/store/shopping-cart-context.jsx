98d458cb452fdf0896db39a21ab6325c7c07579d
Anton Ziuzikov <zuzikov.anton@gmail.com>
2024-02-01 15:06:01 +0300

    feat: use context instead prop and connect context to state

---
+++ src/store/shopping-cart-context.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {createContext} from 'react';
+
+export const CartContext = createContext({
+    items: [],
+    addItemToCart: () => {}, //for autocomletion sake
+});{}
